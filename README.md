522d65d08aafe7b7f5de7e6ce6eddf14bbf74205
rkc <kumarcrishikesh@gmail.com>
2025-01-10 00:56:19 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Spam filtering
+
+## References
+- https://www.youtube.com/watch?v=s74qbeeBb9U&t=3856s
+- https://en.wikipedia.org/wiki/Naive_Bayes_spam_filtering
+- https://github.com/ishmav16/Email-Classification-Spam-or-Ham
---
+++ app/Main.hs
@@ -0,0 +1,53 @@
+module Main where
+
+import Data.Foldable
+import qualified Data.Map as M
+import Data.Maybe
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+import System.Directory
+
+newtype Bow = Bow
+  {bowToMap :: M.Map T.Text Int}
+  deriving (Show, Read)
+
+wordToBow :: T.Text -> Bow
+wordToBow w = Bow $ M.fromList [(w, 1)]
+
+emptyBow :: Bow
+emptyBow = Bow M.empty
+
+instance Semigroup Bow where
+  Bow bow1 <> Bow bow2 = Bow $ M.unionWith (+) bow1 bow2
+
+instance Monoid Bow where
+  mempty = emptyBow
+
+wordsCount :: Bow -> Int
+wordsCount (Bow bow) = sum $ map snd $ M.toList bow
+
+wordProbability :: T.Text -> Bow -> Float
+wordProbability w bow = fromIntegral n / fromIntegral (wordsCount bow)
+ where
+  n = fromMaybe 0 $ M.lookup w $ bowToMap bow
+
+textToBow :: T.Text -> Bow
+textToBow = foldMap wordToBow . T.words
+
+bowFromFile :: FilePath -> IO Bow
+bowFromFile filePath = textToBow <$> T.readFile filePath
+
+bowFromFolder :: FilePath -> IO Bow
+bowFromFolder folderPath = do
+  fileNames <- listDirectory folderPath
+  bows <- mapM (bowFromFile . (folderPath <>)) fileNames
+  pure $ fold bows
+
+spamBow :: IO Bow
+spamBow = bowFromFolder "./data/spam/"
+
+hamBow :: IO Bow
+hamBow = bowFromFolder "./data/ham/"
+
+main :: IO ()
+main = putStrLn "Hello, Haskell!"
